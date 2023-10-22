063d161fb6720498b705085362cc5e458d9416e4
Syota Nishimura <syota4723@gmail.com>
2023-10-22 22:45:58 +0900

    :sparkles: station13までクリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,12 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  // チェックボックスの値取得
+  const checkbox = document.getElementById("check");
+  // 状態取得
+  const isChecked = checkbox.checked;
+  if (isChecked) {
+    document.getElementById("text").style.backgroundColor = "red"
+  } else {
+    document.getElementById("text").style.backgroundColor = "transparent"
+  }
 }
